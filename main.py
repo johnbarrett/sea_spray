58081ac42ad7375ae3c8adce863177e8029c3623
John Barrett <155835+johnbarrett@users.noreply.github.com>
2020-11-21 19:08:23 -1000

    created main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+# load & read csv file
+content = pd.read_csv('data/ssaSlice.csv', header=0)
+
+# converting the old date format from the kite to datetime format
+content['date'] = pd.to_datetime(content['date'])
+content['timedate'] = pd.to_datetime(content['timedate'])
+content['end_time'] = pd.to_datetime(content['end_time'])
+
+
+# next(df)
+# print the csv to the terminal
+# print(content.columns)
+#print(content.loc[[0, 1]], ['date'])
+
+print(content.shape)
+print(content.info())
+
+schema_df = pd.read_csv('data/ssaSlice.csv')
+print(schema_df)
+
+print(content.columns)
+
+subset = content.loc[:, ["lat", "lon"]]
+print(subset)
+
